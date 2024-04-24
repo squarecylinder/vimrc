76f1113ba2951d862be0f0b5cf063d195806816e
jalen-straka <jalen.straka@covermymeds.com>
2024-04-24 16:12:30 -0400

    added igris lol

--- lua/squarecylinder/lazy/colors.lua
+++ lua/squarecylinder/lazy/colors.lua
@@ -1,27 +1,18 @@
 function ColorMyPencils(color)
-	color = color or "dracula"
-	vim.cmd.colorscheme(color)
+    color = color or "dracula"
+    vim.cmd.colorscheme(color)
 
-	vim.cmd("colorscheme dracula")
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-	{
-		"Mofiqul/dracula.nvim",
-		config = function ()
-			require('dracula').setup({
-				transparent_bg = false,
-			})
-
-			ColorMyPencils()
-		end
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "dracula"
-		}
-	},
+    {
+        "Mofiqul/dracula.nvim",
+        config = function()
+            require("dracula").setup({})
+            vim.cmd("colorscheme dracula")
+            ColorMyPencils()
+        end
+    },
 }
